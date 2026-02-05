e3046629ee5d99dc38bca5cfe5a4552b08ec1269
Cian <ciantwomey01@gmail.com>
2026-02-05 15:48:06 +0000

    Add simulated temperature controller

---
+++ src/TempController.cpp
@@ -0,0 +1,49 @@
+#include "TemperatureController.h"
+
+TemperatureController::TemperatureController()
+	: currentTemp(20.0f),
+	  targetTemp(20.0f),
+	  enable(false),
+	  disabled(false) {}
+
+void TemperatureController::enable() {
+	if (!fault) enabled = true;
+}
+
+void TemperatureController::disable() {
+	enabled = false;
+}
+
+void TemperatureController::setTarget(float temperature){
+	if(temperature < MIN_TEMP || temperature > MAX_TEMP) {
+		fault = true;
+		enabled = false;
+		return;
+	}
+	targetTemp = temperature;
+}
+
+float TemperatureController::readTemperature() const {
+	return currentTemp;
+}
+
+bool TemperatureController::isEnabled() const {
+	return enabled;
+}
+
+bool TemperatureController::hasFault() const {
+	return fault;
+}
+
+void TemperatureController::update() {
+	if (!enabled || fault){
+		return;
+	}
+
+	if(currentTemp < targetTemp){
+		currentTemp += 0.5f;
+	}
+	else if(currentTemp > targetTemp){
+		currentTemp -= 0.5f;
+	}
+}
